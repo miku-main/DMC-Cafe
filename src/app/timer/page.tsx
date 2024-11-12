1bd77dc1fe875cc172e3d115c98f462ef20c970b
Gui Sen Zou <125292379+miku-main@users.noreply.github.com>
2024-11-11 20:31:36 -0500

    Added back calender/page.tsx and add HomeIcon to timer/page.tsx

--- src/app/timer/page.tsx
+++ src/app/timer/page.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Timer from './components/Timer';
 import NavBar from '../Home/Home/NavBar';
 import styles from '../Home/Home/HomePage.module.css'
+import HomeIcon from '../Home/Home/HomeIcon'
 
 export default function Page() {
   return (
     <>
       <div className={styles.homePage}>
         <NavBar />
+        <HomeIcon />
         <div>
           <Timer />
         </div>
